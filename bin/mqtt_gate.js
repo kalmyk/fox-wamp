116451d8a5c67c26bf9458bc73bd858eb69c6327
Anatoly Tsapkov <anatoly.tsapkov@gmail.com>
2019-04-04 13:49:24 -0400

    handle mqtt websocket

--- bin/mqtt_gate.js
+++ bin/mqtt_gate.js
@@ -1,20 +1,22 @@
-const MSG = require('../lib/messages')
-const Router = require('../index')
+const http = require('http')
+const url = require('url')
 const program = require('commander')
+const Router = require('../index')
+const MSG = require('../lib/messages')
 
 program
-  .option('-p, --wamp <port>', 'WAMP Server IP port', 9000)
+  .option('-p, --http <port>', 'HTTP Server IP port', 9000)
   .option('-q, --mqtt <port>', 'MQTT Server IP port', 1883)
   .parse(process.argv)
 
-let app = new Router()
-app.setLogTrace(true)
+let router = new Router()
+router.setLogTrace(true)
 
-app.on(MSG.REALM_CREATED, function (realm, realmName) {
+router.on(MSG.REALM_CREATED, function (realm, realmName) {
   console.log('new Relm:', realmName)
 })
 
-app.getRealm('realm1', function (realm) {
+router.getRealm('realm1', function (realm) {
   var api = realm.wampApi()
   api.register('test.foo', function (id, args, kwargs) {
     console.log('called with ', args, kwargs)
@@ -22,7 +24,38 @@ app.getRealm('realm1', function (realm) {
   })
 })
 
-console.log('Listening port wamp', program.wamp, 'mqtt', program.mqtt)
+router.listenMQTT({ port: program.mqtt })
+console.log('MQTT Listening port', program.mqtt)
 
-app.listenMQTT({ port: program.mqtt })
-app.listenWAMP({ port: program.wamp })
+
+const wssWAMP = router.listenWAMP({ noServer: true })
+console.log(`WAMP Web Socket ws://localhost:${program.http}:/wamp`)
+
+const wssMQTT = router.listenWsMQTT({ noServer: true })
+console.log(`MQTT Web Socket ws://localhost:${program.http}:/mqtt`)
+
+let httpServer = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  console.log(req.headers)
+  res.end('hello!');
+})
+
+httpServer.listen(program.http, () => console.log(`HTTP Server Listening on ${program.http}`))
+
+// https://github.com/websockets/ws/pull/885
+httpServer.on('upgrade', (request, socket, head) => {
+  const pathname = url.parse(request.url).pathname;
+
+  if (pathname === '/wamp') {
+    wssWAMP.handleUpgrade(request, socket, head, (ws) => {
+      wssWAMP.emit('connection', ws);
+    });
+  } else if (pathname === '/mqtt') {
+    wssMQTT.handleUpgrade(request, socket, head, (ws) => {
+      wssMQTT.emit('connection', ws);
+    });
+  } else {
+    socket.destroy();
+  }
+})
+
