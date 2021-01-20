9a7eecf93e83d8541b80e51a10e34cf110ba1b08
Anatoly Tsapkov <anatoly.tsapkov@gmail.com>
2021-01-20 01:04:34 -0500

    exclude_me refactoring

--- ext/pubstate.js
+++ ext/pubstate.js
@@ -2,10 +2,13 @@
 
 const NetTransport = require('../lib/hyper/net_transport')
 
-const connection = new NetTransport.ClientSocket({
-  host: process.env.FC_REALM_HOST || '127.0.0.1',
-  port: process.env.FC_REALM_PORT || 9300
-})
+const routerList = {
+  a00 : { host: '127.0.0.1', port: 9300 },
+  a01 : { host: '127.0.0.1', port: 9301 },
+  a02 : { host: '127.0.0.1', port: 9302 },
+}
+
+let cntList = {}
 
 let maxPrefix = undefined
 let maxDate = undefined
@@ -26,16 +29,23 @@ async function worker (session) {
     const result = { date: maxDate, segment: maxSegment }
     console.log('segment', maxPrefix, data, result)
 
-    session.push('takeNewSegment', result)
+    for (let routerId in cntList) {
+      cntList[routerId].push('takeNewSegment', result)
+    }
     task.resolve({})
   })
 }
 
-connection.onopen = function (session) {
-  worker(session).then(function (value) {
-    console.log('worker OK:', value)
-  }, function (err) {
-    console.error('ERROR:', err, err.stack)
-  })
+for (let routerId in routerList) {
+  const settings = routerList[routerId]
+  const connection = new NetTransport.ClientSocket(settings)
+  connection.onopen = function (session) {
+    cntList[routerId] = session
+    worker(session).then(function (value) {
+      console.log('worker OK:', value)
+    }, function (err) {
+      console.error('ERROR:', err, err.stack)
+    })
+  }
+  connection.connect()
 }
-connection.connect()
