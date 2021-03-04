a5bc5f410bf9174a6c4440826310e18c671530d0
Anatoly Tsapkov <anatoly.tsapkov@gmail.com>
2021-03-04 00:57:14 -0500

    db message history

---
+++ test/08.history.js
@@ -0,0 +1,94 @@
+'use strict'
+
+const chai        = require('chai')
+const spies       = require('chai-spies')
+const expect      = chai.expect
+const promised    = require('chai-as-promised')
+
+const Router       = require('../lib/router')
+const {MemBinder}  = require('../lib/mono/membinder')
+const {DbBinder}  = require('../lib/sqlite/dbrouter')
+
+const sqlite3 = require('sqlite3')
+const sqlite = require('sqlite')
+const Msg = require('../lib/sqlite/msg')
+
+chai.use(promised)
+chai.use(spies)
+
+const mkDb = async function () {
+  let db = await sqlite.open({
+    filename: ':memory:',
+    driver: sqlite3.Database
+  })
+  let msg = new Msg(db)
+  await msg.createTables()
+  return new DbBinder(msg)
+}
+
+const runs = [
+  {it: 'mem',  mkBinder: async () => new MemBinder()},
+  {it: 'db',  mkBinder: mkDb },
+]
+
+describe('08. history', function () {
+  runs.forEach(function (run) {
+    describe('event-history:' + run.it, function () {
+      let
+        router,
+        realm,
+        api
+
+      beforeEach(async function () {
+        router = new Router(await run.mkBinder())
+        realm = router.createRealm()
+        router.addRealm('test-realm-name', realm)
+        api = realm.wampApi()
+      })
+    
+      afterEach(function () {
+      })
+  
+      it('receive-event-history:' + run.it, async function () {
+        let expectedData = [
+          {event:'row1'},
+          {event:'row2'},
+          {event:'row3'},
+        ]
+
+        let startPos = await api.publish('test-topic', [], {event:'data'}, {acknowledge: true, trace: true})
+        expect(startPos).to.exist
+
+        await Promise.all([
+          api.publish('test-topic', [], {event:'row1'}, {acknowledge: true, trace: true}),
+          api.publish('test-topic', [], {event:'row2'}, {acknowledge: true, trace: true}),
+          api.publish('test-topic', [], {event:'row3'}, {acknowledge: true, trace: true}),
+        ])
+
+        let subId
+        return new Promise((resolve, reject) => {  
+          return api.subscribe(
+            'test-topic',
+            (id, args, kwargs) => {
+              try {
+                // console.log('#', id, kwargs);
+                expect(kwargs).to.deep.equal(expectedData.shift())
+                if (expectedData.length == 0) {
+                  resolve(subId)
+                }
+              } catch (e) {
+                reject(e)
+              }
+            },
+            {after: startPos}
+          ).then((sub) => {
+            subId = sub
+          })
+        }).then((sub) => {
+          api.unsubscribe(subId)
+        })
+      })
+
+    })
+  })
+})
